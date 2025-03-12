2610f23e254a4af1def489482e09f495cb6172e8
Mahyar Kaliji <mahyar.kaliji@gmail.com>
2025-03-12 13:46:37 +0330

    Feature Base Announcements componenet

---
+++ src/presentation/components/common/Announcements/BaseAnnouncements.tsx
@@ -0,0 +1,30 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import { IBaseAnnouncements } from "./IBaseAnnouncements";
+import {
+  StyledContainer,
+  StyledContextContainer,
+  StyledImgContainer,
+} from "./BaseAnnouncements.style";
+
+const BaseAnnouncements: React.FC<IBaseAnnouncements> = ({
+  title,
+  description,
+  icon,
+}) => {
+  return (
+    <StyledContainer>
+      <StyledImgContainer>{icon}</StyledImgContainer>
+      <StyledContextContainer>
+        <Typography variant="body1" sx={{ color: "neutral.100" }}>
+          {title}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "neutral.200" }}>
+          {description}
+        </Typography>
+      </StyledContextContainer>
+    </StyledContainer>
+  );
+};
+
+export default BaseAnnouncements;
