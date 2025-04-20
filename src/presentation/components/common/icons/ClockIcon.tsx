6a98e50eccc960a0bdb9aa455d2667a996c0677a
Mahyar Kaliji <mahyar.kaliji@gmail.com>
2025-04-20 22:42:46 +0330

    added some icons

--- src/presentation/components/common/icons/ClockIcon.tsx
+++ src/presentation/components/common/icons/ClockIcon.tsx
@@ -1,8 +1,8 @@
 import { CustomSvgProps } from "./IIcons";
 
-export const CloseCircleIcon: React.FC<CustomSvgProps> = ({
+export const ClockIcon: React.FC<CustomSvgProps> = ({
+  size = 24,
   color = "#F7F5FA",
-  size,
 }) => {
   return (
     <svg
@@ -13,21 +13,14 @@ export const CloseCircleIcon: React.FC<CustomSvgProps> = ({
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+        d="M22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12Z"
         stroke={color}
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M9.16992 14.83L14.8299 9.17004"
-        stroke={color}
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M14.8299 14.83L9.16992 9.17004"
+        d="M15.7099 15.18L12.6099 13.33C12.0699 13.01 11.6299 12.24 11.6299 11.61V7.51001"
         stroke={color}
         strokeWidth="1.5"
         strokeLinecap="round"
