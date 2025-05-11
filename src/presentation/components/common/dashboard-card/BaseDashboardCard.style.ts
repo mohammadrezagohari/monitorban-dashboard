33e925c1ebb9093c4965c540522d380713d3b532
Mahyar Kaliji <mahyar.kaliji@gmail.com>
2025-05-11 21:59:38 +0330

    moved icon Wrapper to common folder

--- src/presentation/components/common/dashboard-card/BaseDashboardCard.style.ts
+++ src/presentation/components/common/dashboard-card/BaseDashboardCard.style.ts
@@ -15,18 +15,18 @@ export const CardContainer = styled(Box)<CardContainerProps>(({ theme, width }) 
     }
 }))
 
-export const IconWrapper = styled('span')<IconWrapperProps>(({ theme, bgcolor = "#4D4259", size = 48 }) => ({
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: bgcolor,
-    padding: "12px",
-    borderRadius: "50px",
-    gridRow: "1/3",
-    width: `${size}px`,
-    height: `${size}px`,
-    flexShrink: 0,
-}))
+// export const IconWrapper = styled('span')<IconWrapperProps>(({ theme, bgcolor = "#4D4259", size = 48 }) => ({
+//     display: "flex",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     backgroundColor: bgcolor,
+//     padding: "12px",
+//     borderRadius: "50px",
+//     gridRow: "1/3",
+//     width: `${size}px`,
+//     height: `${size}px`,
+//     flexShrink: 0,
+// }))
 
 export const Divider = styled("hr")(({ theme }) => ({
     border: `1px solid ${theme.palette.text.disabled}`
